96faa8e0274ab4e3b4360d0945fc0bc333e4fc6b
ksbitbusiness <83622223+ksbitbusiness@users.noreply.github.com>
2022-05-16 12:16:26 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var message = "Hello World!";
+var header = document.getElementById("main_header");
+var mainVisual = document.getElementById("main_visual");
+window.onscroll = function () {
+    if (mainVisual.offsetHeight <
+        window.scrollY + header.offsetTop) {
+        header.classList.add("is-scroll");
+    }
+    else {
+        header.classList.remove("is-scroll");
+    }
+};
